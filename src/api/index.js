055f828eb4f440f0a38764278de03178ab104a22
ruanye <55343581@qq.com>
2019-09-03 10:11:29 +0800

    第一个接口

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-
-axios.get('/banner').then((data) => {
-  console.log(data);
-});
+// 响应拦截器
+axios.interceptors.response.use(res => res.data, err => Promise.reject(err));
+// 轮播图请求
+const getBaner = () => axios.get('/banner');
+export { getBaner };
+export default {};
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 // 根目录 vue.config.js
 // webpack 配置 跨域叫做webpack代理
-console.log(1);
 module.exports = {
   devServer: {
     proxy: 'http://localhost:3000',
